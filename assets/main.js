e6fb5f7de8d874cfc311543611c8024070dc1603
FDelgadoArrechea <fdelgadoarrechea@gmail.com>
2017-01-31 11:54:45 +0100

    Primera subida

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+      url: 'https://www.codeschool.com/users/fdelgadoarrechea.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        $.each(response.courses.completed, function(key, value){
+          $('#badges').append('<div class="course"><h3>'+value.title+'</h3><img src="'+value.badge+'"><a href="'+value.url+'" target="_blank" class="btn btn-primary">See course</a></div>');
+        });
+      }
+    });
 });
